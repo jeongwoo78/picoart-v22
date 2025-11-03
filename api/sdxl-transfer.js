b2d406e04894582e334f55299f1162105cc47c52
jeongwoo78 <poly0533@gmail.com>
2025-11-03 13:18:42 +0900

    1

--- api/sdxl-transfer.js
+++ api/sdxl-transfer.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        version: "39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+        version: "7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
         input: {
           image: image,
           prompt: prompt,
